92fe0d736fabd476092d1f0456eaa87d95d558e0
Linda <linda.vieira@cedrotech.com>
2020-08-01 16:27:30 -0300

    Filtering jobs without recursion, using generators instead and implementing more tests

--- app/tests/job_filter/test_get_result_filtered.py
+++ app/tests/job_filter/test_get_result_filtered.py
@@ -1,5 +1,36 @@
 from datetime import datetime
 from app.utils.filter_job_result import get_result_filtered, get_first_subset_that_satisfies_sum
+import pytest
+import random
+
+
+def test_sublist_sum_items_is_less_than_max_allowed_value():
+    jobs = []
+    for i in range(0, 20):
+        expexted_time = random.randint(1, 8)
+        jobs.append({'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                     'maximum_date_finish': datetime(2020, 7, random.randint(1, 31), 17, 22, 35, 181149),
+                     'expexted_time_in_hours_to_finish': expexted_time,
+                     'expected_time_alert_triggered': False, 'status_job': 'CREATED'})
+        jobs = sorted(jobs, key=lambda x: x['maximum_date_finish'])
+        jobs_filtered = get_result_filtered(jobs.copy())
+        for sublist in jobs_filtered:
+            assert sum([job.get('expexted_time_in_hours_to_finish') for job in sublist]) <= 8
+
+
+def test_list_items_will_be_the_same_after_filter():
+    jobs = []
+    for i in range(0, 50):
+        expexted_time = random.randint(1, 8)
+        jobs.append({'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                     'maximum_date_finish': datetime(2020, 7, random.randint(1, 31), 17, 22, 35, 181149),
+                     'expexted_time_in_hours_to_finish': expexted_time,
+                     'expected_time_alert_triggered': False, 'status_job': 'CREATED'})
+        jobs = sorted(jobs, key=lambda x: x['maximum_date_finish'])
+    jobs_filtered = get_result_filtered(jobs.copy())
+    flat_jobs_filtered = [job for sublist in jobs_filtered for job in sublist]
+    flat_jobs_filtered = sorted(flat_jobs_filtered, key=lambda x: x['maximum_date_finish'])
+    assert all(job in jobs for job in flat_jobs_filtered)
 
 
 def test_max_lengh_will_be_respected():
@@ -40,10 +71,6 @@ def test_max_lengh_will_be_respected():
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 19, 17, 22, 35, 181149),
                                    'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'},
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 22, 17, 22, 35, 181149),
-                                   'expexted_time_in_hours_to_finish': 3, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 20, 17, 22, 35, 181149),
@@ -54,21 +81,25 @@ def test_max_lengh_will_be_respected():
                                    'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'},
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 26, 17, 22, 35, 181149),
-                                   'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
+                                   'maximum_date_finish': datetime(2020, 7, 25, 17, 22, 35, 181149),
+                                   'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 21, 17, 22, 35, 181149),
                                    'expexted_time_in_hours_to_finish': 6, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 22, 17, 22, 35, 181149),
+                                   'expexted_time_in_hours_to_finish': 3, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'},
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 26, 17, 22, 35, 181149),
+                                   'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 23, 17, 22, 35, 181149),
                                    'expexted_time_in_hours_to_finish': 8, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 25, 17, 22, 35, 181149),
-                                   'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'},
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 27, 17, 22, 35, 181149),
                                    'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
@@ -147,13 +178,17 @@ def test_sublist_sequential_by_max_date():
                                 'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
                                 'status_job': 'CREATED'},
                                {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                'maximum_date_finish': datetime(2020, 7, 16, 17, 32, 2, 500705),
-                                'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
+                                'maximum_date_finish': datetime(2020, 7, 14, 17, 32, 2, 500705),
+                                'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
                                 'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 9, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 6, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'},
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 16, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 10, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
@@ -167,18 +202,6 @@ def test_sublist_sequential_by_max_date():
                                    'expexted_time_in_hours_to_finish': 8, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 14, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'},
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 18, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'},
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 20, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'}], [
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 15, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 8, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
@@ -187,13 +210,25 @@ def test_sublist_sequential_by_max_date():
                                    'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'},
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 22, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'}], [
+                                   'maximum_date_finish': datetime(2020, 7, 18, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'},
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 19, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 20, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'},
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 21, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 22, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 23, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 2, 'expected_time_alert_triggered': False,
@@ -203,20 +238,16 @@ def test_sublist_sequential_by_max_date():
                                    'expexted_time_in_hours_to_finish': 5, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}], [
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 21, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'},
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                                   'maximum_date_finish': datetime(2020, 7, 27, 17, 32, 2, 500705),
-                                   'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
-                                   'status_job': 'CREATED'}], [
-                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 24, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 4, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'},
                                   {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
                                    'maximum_date_finish': datetime(2020, 7, 25, 17, 32, 2, 500705),
                                    'expexted_time_in_hours_to_finish': 4, 'expected_time_alert_triggered': False,
+                                   'status_job': 'CREATED'}], [
+                                  {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
+                                   'maximum_date_finish': datetime(2020, 7, 27, 17, 32, 2, 500705),
+                                   'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
                                    'status_job': 'CREATED'}]]
     jobs_filtered = get_result_filtered(jobs)
     assert str(jobs_filtered_expected) == str(jobs_filtered)
@@ -254,12 +285,8 @@ def test_get_first_subset_that_satisfies_sum():
              'maximum_date_finish': datetime(2020, 7, 18, 17, 22, 35, 181149), 'expexted_time_in_hours_to_finish': 6,
              'expected_time_alert_triggered': False, 'status_job': 'CREATED'}]
     jobs_sublist_expected = [{'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                              'maximum_date_finish': datetime(2020, 7, 27, 17, 22, 35, 181149),
-                              'expexted_time_in_hours_to_finish': 7, 'expected_time_alert_triggered': False,
-                              'status_job': 'CREATED'},
-                             {'_id': '5f1e498e024f993389691aa6', 'description_job': 'legacy_import',
-                              'maximum_date_finish': datetime(2020, 7, 25, 17, 22, 35, 181149),
-                              'expexted_time_in_hours_to_finish': 1, 'expected_time_alert_triggered': False,
+                              'maximum_date_finish': datetime(2020, 7, 23, 17, 22, 35, 181149),
+                              'expexted_time_in_hours_to_finish': 8, 'expected_time_alert_triggered': False,
                               'status_job': 'CREATED'}]
     jobs_sublist_received = get_first_subset_that_satisfies_sum(jobs, 8)
     assert str(jobs_sublist_expected) == str(jobs_sublist_received)
--- app/utils/filter_job_result.py
+++ app/utils/filter_job_result.py
@@ -1,36 +1,35 @@
-def get_first_subset_that_satisfies_sum(jobs, max_value, sub_jobs=[]):
-    expexted_time_in_hours_to_finish_summed = sum(int(job.get('expexted_time_in_hours_to_finish')) for job in sub_jobs)
-    if expexted_time_in_hours_to_finish_summed == max_value:
-        return sub_jobs
-    if expexted_time_in_hours_to_finish_summed >= max_value:
-        return
+from datetime import datetime
 
-    for index, value in enumerate(jobs):
-        remaining = jobs[index + 1:]
-        result = get_first_subset_that_satisfies_sum(remaining, max_value, sub_jobs + [value])
-        if result:
-            return result
+
+def get_first_subset_that_satisfies_sum(jobs, max_value):
+    aggregated = 0
+    sub_jobs = []
+    while aggregated < 8:
+        try:
+            value = next((job for job in jobs if
+                          job.get('expexted_time_in_hours_to_finish') + aggregated == max_value),
+                         next(job for job in jobs if
+                              job.get('expexted_time_in_hours_to_finish') + aggregated < max_value))
+            sub_jobs.append(value)
+            jobs.remove(value)
+            aggregated += sum([job.get('expexted_time_in_hours_to_finish') for job in sub_jobs])
+        except:
+            break
+    return sub_jobs
 
 
 def get_result_filtered(jobs: list):
-    # Returns a list of sublists that the sum of its elements is less or equal 8.
-
-    # Parameters:
-    #    jobs (list): A list of jobs
-
-    # Returns:
-    #    grouped_jobs_by_window_time(list):The expected list asked in the test project.
     grouped_jobs_by_window_time = []
-    while len(jobs) > 1:
+    while jobs:
         subset = get_first_subset_that_satisfies_sum(jobs, 8)
         if subset:
-            sorted_sub_list = sorted(subset, key=lambda k: k['maximum_date_finish'])
-            grouped_jobs_by_window_time.append(sorted_sub_list)
-            for item in sorted_sub_list:
-                jobs.remove(item)
+            grouped_jobs_by_window_time.append(sorted(subset, key=lambda k: k['maximum_date_finish']))
         else:
-            for item in jobs:
-                grouped_jobs_by_window_time.append([item])
-            break
+            grouped_jobs_by_window_time.append(jobs.copy())
+            for job in jobs.copy():
+                jobs.remove(job)
     return sorted(grouped_jobs_by_window_time, key=lambda x: x[0].get('maximum_date_finish'))
+
+
+
 
